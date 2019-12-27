2c0b0f05347be366d2cee066c579af947cf62c61
Eliud <dev.eliud.trejo@gmail.com>
2019-12-27 11:50:51 -0600

    udpate
    - add swagger info
    - upgrade spring boot/cloud version

--- src/main/java/com/jasypt/api/SwaggerConfig.java
+++ src/main/java/com/jasypt/api/SwaggerConfig.java
@@ -1,5 +1,10 @@
 package com.jasypt.api;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,23 +15,48 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.DocExpansion;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger.web.UiConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+	@Autowired
+	Optional<BuildProperties> build;
+	
+	@Value("${spring.application.name}")
+	private String serviceName;
+	
 	@Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2).select()
-        		.apis(RequestHandlerSelectors.basePackage("com.jasypt.api.controller"))
-            .paths(PathSelectors.regex("/.*"))
-            .build().apiInfo(apiEndPointsInfo());
-    }
-    private ApiInfo apiEndPointsInfo() {
-        return new ApiInfoBuilder().title("Jasypt API")
-            .description("REST API")
-            .contact(new Contact("Eliud Trejo", "", "dev.eliud.trejo@gmail.com"))
-            .version("1.0.0")
-            .build();
-    }
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(apiEndPointsInfo())
+				.select()
+				.apis(RequestHandlerSelectors
+				.basePackage("com.jasypt.api.controller"))
+				.paths(PathSelectors.regex("(/.*)"))
+				.build()
+				.useDefaultResponseMessages(false)
+				.forCodeGeneration(true);
+	}
+
+	private ApiInfo apiEndPointsInfo() {
+		String version = "1.0.0";
+		
+		if (build.isPresent())
+			version = build.get().getVersion();
+		
+		return new ApiInfoBuilder()
+				.title( serviceName )
+				.description("REST API")
+				.contact(new Contact("Eliud Trejo", "", "dev.eliud.trejo@gmail.com"))
+				.version(version).build();
+	}
+
+	@Bean
+	public UiConfiguration uiConfig() {
+		return UiConfigurationBuilder.builder().docExpansion(DocExpansion.LIST).build();
+	}
 }
